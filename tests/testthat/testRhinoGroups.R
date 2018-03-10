e5c5afdf37659d350b4916b3548eba10b2ecb240
Otto from Mac <operdeck@gmail.com>
2018-03-10 18:47:19 +0100

    Fixed

--- tests/testthat/testRhinoGroups.R
+++ tests/testthat/testRhinoGroups.R
@@ -3,6 +3,7 @@ library(rhinotools)
 context("Basic checks")
 test_that("rhinos come in groups", {
   expect_equal(hiRhino(2), "Hello, Rhino Rhino!")
-  expect_equal(hiRhino(), "Hey, no Rhinos!")
+  expect_equal(hiRhino(), "Hello, Rhino!")
+  expect_equal(hiRhino(0), "Hey, no Rhinos!")
   expect_equal(hiRhino(-1), "Oops, Rhinos cant be negative!")
 })
